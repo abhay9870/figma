c259b44e1036818e521dcbf00e7fabe1ae0ba75e
abhaysiddhu1993@gmail.com <abhay.singh@qed42.com>
2022-10-12 09:56:08 +0530

    changes in webform section

--- web/themes/custom/training/js/main.js
+++ web/themes/custom/training/js/main.js
@@ -2,26 +2,17 @@
   Drupal.behaviors.drupalBookOwlCarousel = {
     attach: function (context, settings) {
       jQuery(".search-sec .search-icon").click(function () {
-        var displays = $("#search-block-form").css("visibility");
+        var displays = jQuery("#search-block-form").css("visibility");
         if (displays == "hidden") {
-          $("#search-block-form").css("visibility", "visible");
-          $(".search-icon").addClass("fa-light fa-xmark");
-          $(".search-icon").removeClass("fa fa-search");
+          jQuery("#search-block-form").css("visibility", "visible");
+          jQuery(".search-icon").addClass("fa-light fa-xmark");
+          jQuery(".search-icon").removeClass("fa fa-search");
         } else {
-          $("#search-block-form").css("visibility", "hidden");
-          $(".search-icon").addClass("fa fa-search");
-          $(".search-icon").removeClass("fa-light fa-xmark");
+          jQuery("#search-block-form").css("visibility", "hidden");
+          jQuery(".search-icon").addClass("fa fa-search");
+          jQuery(".search-icon").removeClass("fa-light fa-xmark");
         }
       });
-      // JS for Mobile Menu Toggle
-      // let hamburger = document.querySelector(".hamburger");
-      // let menu = document.querySelector(".nav");
-
-      // hamburger.addEventListener("click", function () {
-      //   hamburger.classList.toggle("isactive");
-      //   menu.classList.toggle("active");
-      // });
-      // JS for Mobile Menu Toggle
     },
   };
 })(jQuery, Drupal);
